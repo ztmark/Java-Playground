4d937e6a8c881f0457b626e71e76fc44cdb99401
Mark <shuyumark@gmail.com>
2015-11-30 15:19:15 +0800

    update

---
+++ src/main/java/com/mark/concurrency/art/Mutex.java
@@ -0,0 +1,89 @@
+package com.mark.concurrency.art;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Author: Mark
+ * Date  : 15/11/30.
+ */
+public class Mutex implements Lock {
+
+    private final Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.tryRelease(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+        // 是否处于占用状态
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            // 当状态为0时获取锁
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalArgumentException();
+            }
+            // 释放锁,将状态设置为0
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        // 返回一个Condition, 每个condition都包含了一个condition队列
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+
+}
