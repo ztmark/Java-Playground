28381aaa66e4a7eff65852f6d7b0d3cf014a957f
Mark <shuyumark@gmail.com>
2016-03-24 21:47:32 +0800

    update

--- src/main/java/com/mark/java8/FunctionalDefaultMethod.java
+++ src/main/java/com/mark/java8/FunctionalDefaultMethod.java
@@ -10,9 +10,29 @@ public interface FunctionalDefaultMethod {
 
     void apply();
 
-    default void defaultMethod() {}
+    default void defaultMethod() {
+        System.out.println("in default method");
+    }
 
-    static void staticMethod() {}
+    static void staticMethod() {
+        System.out.println("in static method");
+    }
 
+
+    public static void main(String[] args) {
+        FunctionalDefaultMethod fdm = () -> {
+            System.out.println("in apply");
+        };
+        fdm.apply();
+        fdm.defaultMethod();
+        FunctionalDefaultMethod.staticMethod();
+//        fdm.staticMethod(); // error
+
+        FunctionalDefaultMethod fdm2 = () -> { staticMethod(); };
+        FunctionalDefaultMethod fdm3 = FunctionalDefaultMethod::staticMethod;
+        fdm3.apply();
+//        FunctionalDefaultMethod fdm4 = () -> { defaultMethod(); }; // non-static method can not referenced from a static context
+
+    }
 
 }
