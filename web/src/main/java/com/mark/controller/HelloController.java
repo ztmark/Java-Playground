01a1af0ebca3b9d637844177bf34f28074e3104b
Mark <shuyumark@gmail.com>
2017-03-21 16:39:02 +0800

    update

--- web/src/main/java/com/mark/controller/HelloController.java
+++ web/src/main/java/com/mark/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.mark.controller;
 
 import com.mark.test.TestFactory;
 import com.mark.test.TestUtil;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloController {
 
+    @Value("${test}")
+    public String test;
+
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public String greeting() {
         final TestUtil o = (TestUtil) TestFactory.newObject();
         o.sayHi();
+        System.out.println(test);
         return "greeting";
     }
 
