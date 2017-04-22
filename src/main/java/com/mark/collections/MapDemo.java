b037c8f8243dcdab4e9016d24593730a4d0ab93b
Mark <shuyumark@gmail.com>
2017-04-22 22:00:01 +0800

    update

--- src/main/java/com/mark/collections/MapDemo.java
+++ src/main/java/com/mark/collections/MapDemo.java
@@ -1,8 +1,11 @@
 package com.mark.collections;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * Author: Mark
@@ -11,6 +14,45 @@ import java.util.Set;
 public class MapDemo {
 
     public static void main(String[] args) {
+//        identityMapDemo();
+//        hasMap();
+//        treeMap();
+        final LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>(32, 0.75F, true);
+        linkedHashMap.put(null, null);
+        linkedHashMap.put("b", null);
+        linkedHashMap.put("a", "a");
+        System.out.println(linkedHashMap.size());
+        System.out.println(linkedHashMap.toString());
+        System.out.println(linkedHashMap.get("b"));
+        System.out.println(linkedHashMap.toString());
+        System.out.println(linkedHashMap.get(null));
+        System.out.println(linkedHashMap.toString());
+    }
+
+    private static void treeMap() {
+        final TreeMap<String, String> treeMap = new TreeMap<>();
+        treeMap.put("a", "a");
+        treeMap.put("b", null);
+        treeMap.put("c", null);
+        System.out.println(treeMap.size());
+        System.out.println(treeMap.toString());
+    }
+
+    private static void hasMap() {
+        final HashMap<String, String> map = new HashMap<>();
+        map.put(null, null);
+        System.out.println(map.size());
+        map.put("a", null);
+        System.out.println(map.size());
+        System.out.println(map.get(null));
+        System.out.println(map.get("a"));
+        map.put(null, null);
+        System.out.println(map.size());
+        map.put("b", null);
+        System.out.println(map.size());
+    }
+
+    private static void identityMapDemo() {
         IdentityHashMap<Value, String> im = new IdentityHashMap<>();
         final Value value = new Value(2, 3);
         final Value value1 = new Value(3, 2);
