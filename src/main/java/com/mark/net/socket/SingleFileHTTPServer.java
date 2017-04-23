ee6959f84c6c68f235de497e1dcac3e52cd89c6f
Mark <shuyumark@gmail.com>
2017-04-23 11:38:06 +0800

    update

--- src/main/java/com/mark/net/socket/SingleFileHTTPServer.java
+++ src/main/java/com/mark/net/socket/SingleFileHTTPServer.java
@@ -1,8 +1,11 @@
 package com.mark.net.socket;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
@@ -17,6 +20,8 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.google.common.base.Strings;
+
 /**
  * Author: Mark
  * Date  : 2017/4/22
@@ -25,11 +30,10 @@ public class SingleFileHTTPServer {
 
     private final byte[] content;
     private final byte[] header;
-    private final int port = 8680;
-    private final String encoding = StandardCharsets.UTF_8.toString();
 
     public SingleFileHTTPServer(byte[] data, String mimeType) throws UnsupportedEncodingException {
         content = data;
+        String encoding = StandardCharsets.UTF_8.toString();
         String hdr = "HTTP/1.1 200 OK\r\n" +
                 "Server: OneFile 2.0\r\n" +
                 "Content-length: " + this.content.length + "\r\n" +
@@ -39,8 +43,9 @@ public class SingleFileHTTPServer {
 
     private void start(){
         ExecutorService pool = Executors.newFixedThreadPool(100);
+        int port = 8680;
         try (ServerSocket server = new ServerSocket(port)) {
-            System.out.println("Acceting connections on port " + server.getLocalPort());
+            System.out.println("Accepting connections on port " + server.getLocalPort());
             while (true) {
                 try {
                     final Socket connection = server.accept();
@@ -66,14 +71,21 @@ public class SingleFileHTTPServer {
         public Void call() throws Exception {
             try {
                 OutputStream out = new BufferedOutputStream(connection.getOutputStream());
-                final InputStream in = connection.getInputStream();
+//                final InputStream in = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(connection.getInputStream())));
                 StringBuilder request = new StringBuilder(100);
                 while (true) {
-                    int c = in.read();
-                    if (c == '\r' || c == '\n' || c == -1) {
+//                    int c = in.read();
+//                    if (c == '\r' || c == '\n' || c == -1) {
+//                        break;
+//                    }
+//                    request.append((char) c);
+                    final String line = reader.readLine();
+                    System.out.println(line);
+                    if (Strings.isNullOrEmpty(line)) {
                         break;
                     }
-                    request.append((char) c);
+                    request.append(line);
                 }
                 if (request.toString().contains("HTTP/")) {
                     out.write(header);
