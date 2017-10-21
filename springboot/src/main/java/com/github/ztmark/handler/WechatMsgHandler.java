e3b019983b1146ef9f333af248ee05793cec63fc
Mark <shuyumark@gmail.com>
2017-10-21 16:35:27 +0800

    update

--- springboot/src/main/java/com/github/ztmark/handler/WechatMsgHandler.java
+++ springboot/src/main/java/com/github/ztmark/handler/WechatMsgHandler.java
@@ -2,12 +2,15 @@ package com.github.ztmark.handler;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpMessageHandler;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.kefu.WxMpKefuMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 
@@ -17,8 +20,19 @@ import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
  */
 @Component
 public class WechatMsgHandler implements WxMpMessageHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WechatMsgHandler.class);
+
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService, WxSessionManager sessionManager) throws WxErrorException {
+        LOGGER.info("WechatMsgHandler.handle {}", wxMessage);
+        System.out.println(wxMessage);
+        final String openId = wxMessage.getFromUser();
+        final WxMpKefuMessage msg = WxMpKefuMessage.TEXT().toUser(openId).content("welcome to ztmark").build();
+        final boolean b = wxMpService.getKefuService().sendKefuMessage(msg);
+        if (b) {
+            System.out.println("success send msg back");
+        }
         return null;
     }
 }
