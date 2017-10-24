e2efc3b04b2ec14ff92015fc8caafa169e5638b3
Mark <shuyumark@gmail.com>
2017-10-24 22:07:11 +0800

    update

---
+++ code-kata/src/main/java/com/mark/codewars/YesOrNo.java
@@ -0,0 +1,13 @@
+package com.mark.codewars;
+
+/**
+ * @author Mark
+ * @date 2017/10/24
+ */
+public class YesOrNo {
+
+    public static String boolToWord(boolean b) {
+        return b ? "Yes" : "No";
+    }
+
+}
