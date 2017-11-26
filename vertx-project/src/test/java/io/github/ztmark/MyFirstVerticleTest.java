1ed34174828e3dc77730d8ae6a1210abb93df481
Mark <shuyumark@gmail.com>
2017-11-26 14:08:47 +0800

    update

--- vertx-project/src/test/java/io/github/ztmark/MyFirstVerticleTest.java
+++ vertx-project/src/test/java/io/github/ztmark/MyFirstVerticleTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -47,5 +48,37 @@ public class MyFirstVerticleTest {
         });
     }
 
+    @Test
+    public void testCheckThatTheIndexPageIsServed(TestContext context) {
+        final Async async = context.async();
+        vertx.createHttpClient().getNow(port, "localhost", "/assets/index.html", response -> {
+            context.assertEquals(200, response.statusCode());
+            context.assertEquals("text/html;charset=UTF-8", response.getHeader("content-type"));
+            response.bodyHandler(body -> {
+                context.assertTrue(body.toString().contains("<title>My Whisky Collection</title>"));
+                async.complete();
+            });
+        });
+    }
+
+    @Test
+    public void testThatWeCanAdd(TestContext context) {
+        final String json = Json.encodePrettily(new Whisky("test1", "test2"));
+        final Async async = context.async();
+        vertx.createHttpClient().post(port, "localhost", "/api/whiskies")
+             .putHeader("content-type", "application/json")
+             .putHeader("content-length", String.valueOf(json.length()))
+             .handler(response -> {
+                 context.assertEquals(201, response.statusCode());
+                 context.assertTrue(response.getHeader("content-type").contains("application/json"));
+                 response.bodyHandler(body -> {
+                     final Whisky whisky = Json.decodeValue(body, Whisky.class);
+                     context.assertEquals("test1", whisky.getName());
+                     context.assertEquals("test2", whisky.getOrigin());
+                     context.assertNotNull(whisky.getId());
+                     async.complete();
+                 });
+             }).write(json).end();
+    }
 
 }
